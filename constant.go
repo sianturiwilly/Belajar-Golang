9551d9d8585e24f3ab2cc6a6da1598d4b5377a34
sianturiwilly <prajudiwilliam10@gmail.com>
2022-12-20 18:39:17 +0700

    Commit kedua.

---
+++ constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "Willi"
+	const lastName = "Sianturi"
+	const value = 1000
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
--- variable.go
+++ variable.go
@@ -10,4 +10,21 @@ func main() {
 
 	name = "Prajudi William"
 	fmt.Println(name)
+
+	var friendName = "Darwis"
+	fmt.Println(friendName)
+
+	var age = 29
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Willi"
+		lastName  = "Sianturi"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
 }
