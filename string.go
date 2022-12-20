7989bf680df858e6823156ed2380c20f6f580c28
sianturiwilly <prajudiwilliam10@gmail.com>
2022-12-20 18:10:13 +0700

    first commit

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Willi")
+	fmt.Println("Willi Chrisdeardo")
+	fmt.Println("Willi Chrisdeardo Sianturi")
+
+	fmt.Println(len("Willi"))
+	fmt.Println("Willi Chrisdeardo"[0])
+	fmt.Println("Willi Chrisdeardo Sianturi"[1])
+}
---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Willi Sianturi"
+	fmt.Println(name)
+
+	name = "Prajudi William"
+	fmt.Println(name)
+}
